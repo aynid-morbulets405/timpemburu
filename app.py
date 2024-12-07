2d097870e694fa7dd458df0af4b4c7782122786d
Anung B Ariwibowo <barliant@gmail.com>
2024-12-07 13:35:10 +0700

    Template script proyek.

--- app.py
+++ app.py
@@ -10,3 +10,25 @@ st.title("timpemburu")
 #fathur
 
 #frendi
+st.write("# Tugas Kelompok Tim Pemburu")
+
+st.write("## Pendahuluan")
+st.write("Tuliskan di bagian ini latar belakang data apa yang dipilih, mengapa kelompok memilih data ini, dsb.")
+
+st.write("## Deskripsi Data")
+st.write("Tuliskan di bagian ini deskripsi tentang data yang digunakan.")
+
+st.write("## Visualisasi")
+st.write("Buat visualisasi yang menurut kelompok kalian perlu ditampilkan.")
+st.write("Gunakan juga elemen-elemen interaktif `streamlit`.")
+
+st.write("## Analisis")
+st.write("Buat analisis sederhana dari visualisasi data yang muncul di bagian sebelumnya.")
+
+st.write("## Kesimpulan")
+st.write("Tuliskan butir-butir kesimpulan dari analisis.")
+
+st.write("## Referensi / Daftar Pustaka")
+st.write("Tuliskan di bagian ini referensi yang digunakan dalam proyek kelompok ini, misalnya sumber data, makalah ilmiah, dsb.")
+
+
