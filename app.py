0412fff63b4570ce176f41e77bf1223230bbf509
Anung B Ariwibowo <barliant@gmail.com>
2024-12-09 15:47:34 +0700

    Modify app.py

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ import plotly.express as px
 st.title("timpemburu")
 
 
-#andhika
-
 #fathur
-
-#frendi
 st.write("# Tugas Kelompok Tim Pemburu")
 
 st.write("## Pendahuluan")
@@ -18,6 +14,7 @@ st.write("Tuliskan di bagian ini latar belakang data apa yang dipilih, mengapa k
 st.write("## Deskripsi Data")
 st.write("Tuliskan di bagian ini deskripsi tentang data yang digunakan.")
 
+#andhika
 st.write("## Visualisasi")
 st.write("Buat visualisasi yang menurut kelompok kalian perlu ditampilkan.")
 st.write("Gunakan juga elemen-elemen interaktif `streamlit`.")
@@ -25,6 +22,7 @@ st.write("Gunakan juga elemen-elemen interaktif `streamlit`.")
 st.write("## Analisis")
 st.write("Buat analisis sederhana dari visualisasi data yang muncul di bagian sebelumnya.")
 
+#frendi
 st.write("## Kesimpulan")
 st.write("Tuliskan butir-butir kesimpulan dari analisis.")
 
