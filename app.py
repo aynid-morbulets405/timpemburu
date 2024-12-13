fd2b2c7a094ba5e65b0272de7a305fa43bce2936
andhika <tonimarjani06@gmail.com>
2024-12-13 09:13:55 +0700

    mengupdate file app.py

--- app.py
+++ app.py
@@ -5,8 +5,26 @@ import plotly.express as px
 st.title("timpemburu")
 
 
-#andhika
-
 #fathur
+st.write("# Tugas Kelompok Tim Pemburu")
+
+st.write("## Pendahuluan")
+st.write("kita mgeambil data tentang.")
+
+st.write("## Deskripsi Data")
+st.write("Tuliskan di bagian ini deskripsi tentang data yang digunakan.")
+
+#andhika
+st.write("## Visualisasi")
+st.write("Buat visualisasi yang menurut kelompok kalian perlu ditampilkan.")
+st.write("Gunakan juga elemen-elemen interaktif `streamlit`.")
+
+st.write("## Analisis")
+st.write("Buat analisis sederhana dari visualisasi data yang muncul di bagian sebelumnya.")
 
 #frendi
+st.write("## Kesimpulan")
+st.write("Tuliskan butir-butir kesimpulan dari analisis.")
+
+st.write("## Referensi / Daftar Pustaka")
+st.write("Tuliskan di bagian ini referensi yang digunakan dalam proyek kelompok ini, misalnya sumber data, makalah ilmiah, dsb.")
