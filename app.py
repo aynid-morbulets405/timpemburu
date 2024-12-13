ed1ec5a80fb18a59e6c2074d96ed020678e36309
andhika <tonimarjani06@gmail.com>
2024-12-13 10:10:12 +0700

    menambahkan file app.py

--- app.py
+++ app.py
@@ -9,7 +9,15 @@ st.title("timpemburu")
 st.write("# Tugas Kelompok Tim Pemburu")
 
 st.write("## Pendahuluan")
-st.write("kita mgeambil data tentang.")
+st.write("Bitcoin merupakan salah satu bentuk mata uang digital atau cryptocurrency yang diciptakan oleh seseorang atau kelompok yang menggunakan nama samaran Satoshi Nakamoto pada tahun 2009. Sebagai mata uang berbasis teknologi blockchain, Bitcoin menawarkan mekanisme transaksi yang bersifat desentralisasi, transparan, dan aman tanpa memerlukan perantara seperti bank atau lembaga keuangan tradisional. Hal ini menjadikan Bitcoin sebagai salah satu inovasi finansial paling revolusioner di era digital.
+
+Dalam satu dekade terakhir, Bitcoin telah menarik perhatian luas dari berbagai kalangan, mulai dari individu, investor, hingga institusi global. Perannya tidak hanya sebagai alat pembayaran, tetapi juga sebagai instrumen investasi yang dianggap memiliki potensi keuntungan besar. Namun, di sisi lain, volatilitas harga yang tinggi, risiko keamanan, serta tantangan regulasi menjadi isu-isu yang kerap menyertai pengembangan dan adopsi Bitcoin.
+
+Sebagai objek penelitian, Bitcoin menawarkan peluang untuk mengeksplorasi berbagai aspek, termasuk teknologi blockchain yang mendasarinya, dinamika pasar, implikasi ekonominya, serta dampaknya terhadap sistem keuangan global. Penelitian tentang Bitcoin juga dapat memberikan wawasan tentang bagaimana mata uang digital dapat memengaruhi perilaku konsumen, strategi investasi, dan kebijakan publik.
+
+Dengan latar belakang tersebut, penelitian ini bertujuan untuk menganalisis lebih dalam tentang Bitcoin, baik dari segi teknis, ekonomi, maupun sosial. Melalui pendekatan multidisiplin, diharapkan penelitian ini dapat memberikan kontribusi signifikan dalam memahami fenomena Bitcoin dan potensi masa depannya sebagai bagian dari ekosistem keuangan digital.
+
+")
 
 st.write("## Deskripsi Data")
 st.write("Tuliskan di bagian ini deskripsi tentang data yang digunakan.")
